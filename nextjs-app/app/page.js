af1ec12e577a0469a2b8ccdaa0fe2c09e146756b
fesabelilla <fesabelilla@gmail.com>
2025-05-17 15:43:51 +0600

    Pages & Layouts | Routing & Navigation | Loading State

---
+++ nextjs-app/app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="mt-10">
+      <div>
+        Rokomari.com is a Bangladeshi online bookshop and e-commerce platform,
+        initially launched in 2012 as an online bookstore. It has since expanded
+        to offer a wide variety of products beyond books, including CDs/DVDs,
+        sports jerseys, stationery items, and more. Rokomari is known for its
+        extensive book collection, with over 300,000 titles and a large number
+        of categories. It also offers services like Cash on Delivery and a
+        strong customer support system.
+      </div>
+    </main>
+  );
+}

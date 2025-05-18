450d0fd4235121677690af3dc878429159392a86
fesabelilla <fesabelilla@gmail.com>
2025-05-18 15:35:34 +0600

    Item Not found added

---
+++ nextjs-app/app/blogs/[id]/not-found.js
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <div>Item Not Found</div>;
+}
--- nextjs-app/app/blogs/[id]/page.js
+++ nextjs-app/app/blogs/[id]/page.js
@@ -1,4 +1,11 @@
+import NotFound from "./not-found";
+
 export default async function BlogPage({ params }) {
   const { id } = await params;
+
+  if (id === 3) {
+    NotFound();
+  }
+
   return <div className="mt-6">This is blog page {id}</div>;
 }

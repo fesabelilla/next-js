fe67ba8bc2168c43e9f31d1ab7a5338c5dffe50f
fesabelilla <fesabelilla@gmail.com>
2025-05-20 14:02:46 +0600

    Image Added and Optimization Included

--- nextjs-app/app/about/mission/page.js
+++ nextjs-app/app/about/mission/page.js
@@ -1,4 +1,6 @@
 import Button from "@/app/components/Button";
+import thumb from "@/public/images/img1.jpeg";
+import Image from "next/image";
 
 export default function Mission() {
   return (
@@ -9,6 +11,7 @@ export default function Mission() {
         Bangladeshs most comprehensive virtual business that helps customers
         make informed choices and extract the best value for their money.
       </div>
+      <Image src={thumb} alt="Thumb Image" />
       <Button />
     </main>
   );

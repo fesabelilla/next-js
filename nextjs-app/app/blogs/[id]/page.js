fd601fd58aba3d04640f1bd4fe298acddc2439b7
fesabelilla <fesabelilla@gmail.com>
2025-05-20 12:19:38 +0600

    Not Found Issue Fix

--- nextjs-app/app/blogs/[id]/page.js
+++ nextjs-app/app/blogs/[id]/page.js
@@ -1,10 +1,10 @@
-import NotFound from "./not-found";
+import { notFound } from "next/navigation";
 
 export default async function BlogPage({ params }) {
   const { id } = await params;
 
-  if (id === 3) {
-    NotFound();
+  if (id === "3") {
+    notFound();
   }
 
   return <div className="mt-6">This is blog page {id}</div>;

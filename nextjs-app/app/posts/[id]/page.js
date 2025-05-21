40dff9526e6720bf3a3111c8040f06ad657de1a3
fesabelilla <fesabelilla@gmail.com>
2025-05-21 11:46:51 +0600

    Progressive Rendering

--- nextjs-app/app/posts/[id]/page.js
+++ nextjs-app/app/posts/[id]/page.js
@@ -1,4 +1,7 @@
+import Comments from "@/app/components/Comments";
 import getPost from "@/app/lib/getPost";
+import getPostComment from "@/app/lib/getPostComment";
+import { Suspense } from "react";
 
 export async function generateMetadata({ params }) {
   const { id } = await params;
@@ -12,12 +15,22 @@ export async function generateMetadata({ params }) {
 
 export default async function PostPage({ params }) {
   const { id } = await params;
-  const post = await getPost(id);
+  const postPromise = getPost(id);
+  const commentsPromise = getPostComment(id);
+
+  //   const [post, comments] = await Promise.all([postPromise, commentsPromise]);
+
+  const post = await postPromise;
 
   return (
     <div className="mt-6">
       <h2 className="text-blue-500">{post.title}</h2>
       <p className="mt-3">{post.body}</p>
+
+      <hr />
+      <Suspense fallback="<h1>Loading Comments....</h1>">
+        <Comments promise={commentsPromise} />
+      </Suspense>
     </div>
   );
 }
